75094a96da521d0393ab72001a94eeeee0e88e6b
Dennis Lee <dennis.l@digipen.edu>
2018-05-10 18:29:11 -0700

    Challenge completed

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
+console.log("Start program");
+
 alert("Help me fix this program!");
-alert(Can you get this message to appear?);
+alert("Can you get this message to appear?");
 document.write("<h2>My first JavaScript program</h2>");
-document.wrong("<p>I'm practicing 'debugging'.</p>");
+document.write("<p>I'm practicing 'debugging'.</p>");
+
+console.log("End program");
--- tmp-scripts.js
+++
@@ -1,3 +0,0 @@
-alert("Hello");
-document.write("<h1>Welcome to JS Basics</h1>");
-alert("Thanks for visiting");
