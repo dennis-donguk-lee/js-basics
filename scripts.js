84aac3f31ef9c93ac6add654a75fa66bc44ecf82
Dennis Lee <dennis.l@digipen.edu>
2018-05-10 18:43:36 -0700

    Capturing visitor input

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 console.log("Start program");
 
-alert("Help me fix this program!");
-alert("Can you get this message to appear?");
-document.write("<h2>My first JavaScript program</h2>");
-document.write("<p>I'm practicing 'debugging'.</p>");
+//alert("Help me fix this program!");
+//alert("Can you get this message to appear?");
+//document.write("<h2>My first JavaScript program</h2>");
+//document.write("<p>I'm practicing 'debugging'.</p>");
+
+var visitorName = prompt("Name?");
+alert(visitorName);
 
 console.log("End program");
